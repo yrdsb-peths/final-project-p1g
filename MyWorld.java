80a44b56c4bfddc6a6e09816919d6ca471963047
marissa <335510731@gapps.yrdsb.ca>
2021-12-17 08:54:15 -0500

    Revert "testingg"
    This reverts commit 01a7ef1e00d2853559f88bf946e69de42ba80800.

--- MyWorld.java
+++ MyWorld.java
@@ -17,7 +17,5 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        int i = 5 / (1+2);
     }
 }
