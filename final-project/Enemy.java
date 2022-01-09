2ae4ed4000aaffde656b307f4790a8e64e12e232
BofaSchmucosa <93140624+BofaSchmucosa@users.noreply.github.com>
2022-01-09 15:25:40 -0500

    Created some worlds n actors

---
+++ final-project/Enemy.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author Sherman, Marissa, and Carmen
+ * @version 09/01/22
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ final-project/GameWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameWorld here.
+ * 
+ * @author Sherman, Marissa, and Carmen
+ * @version 09/01/22
+ */
+public class GameWorld extends World
+{
+
+    /**
+     * Constructor for objects of class GameWorld.
+     * 
+     */
+    public GameWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
